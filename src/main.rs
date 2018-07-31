e9fbe90a8abbb2b200159ed46eea357bcff6ba48
Herrera93 <yiyogold@gmail.com>
2018-07-31 09:28:04 -0700

    Added info level to logs

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ use actix_web::{server, App, HttpRequest};
 extern crate log;
 extern crate simple_logger;
 
+use log::Level;
+
 fn index(_req: &HttpRequest) -> &'static str {
     info!("Executing index");
     "Hello Joylabs from Rust!"
 }
 
 fn main() {
-    simple_logger::init().unwrap();
+    simple_logger::init_with_level(Level::Info).unwrap();
 
     info!("Starting server on localhost:80");
     server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:80")
+        .bind("localhost:80")
         .unwrap()
         .run();
 }
